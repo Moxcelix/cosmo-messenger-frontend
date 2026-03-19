5edecd7e83bd83c13ee475332f47531cd48d0068
moxcelix <maxim.spf@gmail.com>
2026-03-19 16:48:20 +0300

    add chat models

---
+++ src/types/models/Chat.ts
@@ -0,0 +1,34 @@
+export interface Member{
+    user_id: string
+    role: string
+    joined_at: string
+}
+
+export interface Chat {
+    id: string
+    type: string
+    name: string
+    avatar_url: string
+    description: string
+    created_at: string
+    updated_at: string
+    members: Member[]
+}
+
+export interface Message{
+    id: string
+    chat_id: string
+    sender_id: string
+    content: string
+    reply_to_id: string
+    edited: boolean
+    context_type: string
+    created_at: string
+}
+
+export interface User{
+    user_id: string
+    username: string
+    display_name: string
+    avatar_url: string
+}
